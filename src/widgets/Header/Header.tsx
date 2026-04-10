edf97ab0f4cc01f3bb652a299529608670817508
Anastasiia Ustin <2anastasiia@mail.ru>
2026-04-10 18:49:18 +0200

    added classnames to the elements

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,12 +1,13 @@
 import AppLink from "@/shared/ui/AppLink/AppLink";
 import { FC } from "react";
+import styles from './Header.module.css'
 
 const Header: FC = () => {
   return (
-    <header>
-      <nav>
-        <AppLink to={"/"}>Все котики</AppLink>
-        <AppLink to={"/favourites"}>Любимые котики</AppLink>
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <AppLink className={styles.link} to={"/"}>Все котики</AppLink>
+        <AppLink className={styles.link} to={"/favourites"}>Любимые котики</AppLink>
       </nav>
     </header>
   );
