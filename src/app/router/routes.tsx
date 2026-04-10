b044c71b94c5ef84192b224c73dec7e86f72afd4
Anastasiia Ustin <2anastasiia@mail.ru>
2026-04-10 20:56:58 +0200

    created routes

---
+++ src/app/router/routes.tsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter } from "react-router-dom"
+import MainLayout from "../layout"
+import HomePage from "@/pages/HomePage"
+import FavouriteCatsPage from "@/pages/FavouriteCatsPage"
+import NotFoundPage from "@/pages/NotFoundPage"
+
+const router = createBrowserRouter([{
+    path: '/',
+    element: <MainLayout/>,
+    children: [
+        {
+            path: '',
+            element: <HomePage/>
+        },
+        {
+            path: 'favourites',
+            element: <FavouriteCatsPage/>
+        },
+        {
+            path: '*',
+            element: <NotFoundPage/>
+        },
+    ]
+    
+}])
+
+export default router
