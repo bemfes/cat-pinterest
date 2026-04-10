20661de94ea505bd8cea52142152519ea373860e
Anastasiia Ustin <2anastasiia@mail.ru>
2026-04-10 20:46:46 +0200

    created basic Favourite Cats Page

---
+++ src/pages/FavouriteCatsPage/FavouriteCatsPage.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react"
+
+const FavouriteCatsPage: FC = () => {
+    return (
+        <div>
+            favourite cats
+        </div>
+    )
+}
+
+export default FavouriteCatsPage
