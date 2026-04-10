f79782a97c25d392acfc43ec188623337e93febf
Anastasiia Ustin <2anastasiia@mail.ru>
2026-04-10 21:14:04 +0200

    renamed main.tsx and moved to app folder

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './app/App.tsx'
+import App from './App'
+import AppProviders from './providers/AppProviders.tsx'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AppProviders>
+      <App />
+    </AppProviders>
   </StrictMode>,
 )
