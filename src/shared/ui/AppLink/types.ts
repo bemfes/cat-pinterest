04b648a9d9b30dadc69c0e5201421d9805d94186
Anastasiia Ustin <2anastasiia@mail.ru>
2026-04-10 22:02:03 +0200

    made union of string and function types for classname type

--- src/shared/ui/AppLink/types.ts
+++ src/shared/ui/AppLink/types.ts
@@ -3,5 +3,5 @@ import React from "react";
 export interface AppLinkProps {
     to: string;
     children: React.ReactNode;
-    className?: (isActive: { isActive: boolean }) => string;
+    className?: string | ((isActive: { isActive: boolean }) => string);
 }
