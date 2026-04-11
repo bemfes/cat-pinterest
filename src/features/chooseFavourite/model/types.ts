e1f9c71befef0fbe2a51d6de1e4ac72c739104a7
Anastasiia Ustin <2anastasiia@mail.ru>
2026-04-11 18:13:38 +0200

    created type for context

--- src/features/chooseFavourite/model/types.ts
+++ src/features/chooseFavourite/model/types.ts
@@ -1,3 +1,9 @@
+import type { CatItemType } from "@/entities/cat/model"
+
 export interface FavouriteCatsContextProviderProps {
     children: React.ReactNode
+}
+
+export interface FavouriteCatsContextProps {
+    favouriteCats: CatItemType[]
 }
