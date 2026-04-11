d4bbdfff41ba90b264c12147b7bafadd4e08de4f
Anastasiia Ustin <2anastasiia@mail.ru>
2026-04-11 19:08:59 +0200

    added real function from context to onClick

--- src/features/chooseFavourite/ui/LikeButton/LikeButton.tsx
+++ src/features/chooseFavourite/ui/LikeButton/LikeButton.tsx
@@ -3,10 +3,12 @@ import { FC } from "react"
 import HeartIcon from "@/shared/assets/icons/heart-icon.svg?react"
 import styles from './LikeButton.module.css'
 import { LikeButtonProps } from "./types"
+import { useFavouriteCatsContext } from "../../model"
 
 const LikeButton: FC<LikeButtonProps> = ({id, url}) => {
+  const { handleFavouriteCat } = useFavouriteCatsContext()
   return (
-    <Button className={styles.button} onClick={() => console.log()}>
+    <Button className={styles.button} onClick={() => handleFavouriteCat({url: url, id: id})}>
       <HeartIcon className={styles.heart} />
     </Button>
   )
