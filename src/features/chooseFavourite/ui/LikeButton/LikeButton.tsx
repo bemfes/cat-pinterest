4dd79672f6bf6c8f2aa42e002df99ad301502110
Anastasiia Ustin <2anastasiia@mail.ru>
2026-04-11 19:06:02 +0200

    added props and their type

--- src/features/chooseFavourite/ui/LikeButton/LikeButton.tsx
+++ src/features/chooseFavourite/ui/LikeButton/LikeButton.tsx
@@ -2,8 +2,9 @@ import Button from "@/shared/ui/Button"
 import { FC } from "react"
 import HeartIcon from "@/shared/assets/icons/heart-icon.svg?react"
 import styles from './LikeButton.module.css'
+import { LikeButtonProps } from "./types"
 
-const LikeButton: FC = () => {
+const LikeButton: FC<LikeButtonProps> = ({id, url}) => {
   return (
     <Button className={styles.button} onClick={() => console.log()}>
       <HeartIcon className={styles.heart} />
