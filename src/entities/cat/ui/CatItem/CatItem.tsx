f14d9dddae19dd96a0324c7d3e7cdfa1407e8913
Anastasiia Ustin <2anastasiia@mail.ru>
2026-04-11 17:32:15 +0200

    added wrapper div for children

--- src/entities/cat/ui/CatItem/CatItem.tsx
+++ src/entities/cat/ui/CatItem/CatItem.tsx
@@ -1,12 +1,15 @@
 import { FC } from "react"
 import { CatItemProps } from "./types"
+import styles from './CatItem.module.css'
 
 
 const CatItem: FC<CatItemProps> = ({url, children}) => {
     return (
-       <div>
-        <img src={url} alt="adorable cat" />
-        {children}
+       <div className={styles.item}>
+        <img className={styles.img} src={url} alt="adorable cat" />
+        <div className={styles.wrapper}>
+            {children}
+        </div>
        </div>
     )
 }
