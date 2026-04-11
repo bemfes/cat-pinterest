14624c44c2052ce603e1db6df1a02e6033cf3507
Anastasiia Ustin <2anastasiia@mail.ru>
2026-04-11 14:47:51 +0200

    created hook for getting cats images with loading states

---
+++ src/widgets/CatFeed/model/useGetCats.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react"
+import { CatItem } from "@/entities/cat/model"
+import { getCats } from "@/entities/cat/api"
+
+const useGetCats = () => {
+
+    const [data, setData] = useState<CatItem[]>([])
+    const [loading, setLoading] = useState<boolean>(false)
+    const [error, setError] = useState<null | string>(null)
+    const [page, setPage] = useState<number>(0)
+    const [totalImages, setTotalImages] = useState<number>(0)
+
+    
+    useEffect(() => {
+        async function getCatsImages() {
+          try {
+
+            setLoading(true)
+            setError(null)
+            
+            const { catData, totalCountImages } = await getCats(page, 15)
+
+            setData(prev => [ ...prev, ...catData ])
+
+            setTotalImages(totalCountImages)
+          
+          } catch (error: unknown) {
+
+            if (error instanceof Error) {
+              setError(error.message)
+            } else {
+            setError('Unknown error')
+          }
+
+        } finally {
+          setLoading(false)
+        }
+      }
+      
+      getCatsImages()
+      
+    }, [page])
+
+    return { data, loading, error, totalImages, setPage }
+}
+
+export default useGetCats
