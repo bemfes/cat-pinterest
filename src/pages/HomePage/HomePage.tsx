e300e50f65bcdaee510efc92b0ac1caeaf711bb8
Anastasiia Ustin <2anastasiia@mail.ru>
2026-04-11 14:49:47 +0200

    added Cat Feed widget

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,11 @@
+import CatFeed from "@/widgets/CatFeed/ui/CatFeed"
 import { FC } from "react"
 
 const HomePage: FC = () => {
     return (
-        <div>
-          homepage  
-        </div>
+        
+        <CatFeed/>
+        
     )
 }
 
