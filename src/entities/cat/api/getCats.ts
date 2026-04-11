214350b36e42da745ad60a13189cfcdafb4c9a1f
Anastasiia Ustin <2anastasiia@mail.ru>
2026-04-11 14:45:59 +0200

    created function for api get requests

---
+++ src/entities/cat/api/getCats.ts
@@ -0,0 +1,23 @@
+import { CAT_API_URL } from "@/shared/constants"
+import { CatApiResponse } from "./types"
+import { CatItem } from "../model"
+
+export const getCats = async (page: number, limit: number): Promise<{ catData: CatItem[], totalCountImages: number}> => {
+    const res: Response = await fetch(
+        `${CAT_API_URL}?page=${page}&limit=${limit}`,
+         {
+            headers: {
+              "Content-Type": "application/json",
+              "x-api-key": import.meta.env.VITE_API_KEY
+         }
+        })
+    if (!res.ok) {
+        throw new Error('Failed to fetch')
+    }
+    const responseData: CatApiResponse[] = await res.json()
+    const catData: CatItem[] = responseData.map(cat => ({url: cat.url, id: cat.id}))
+    const totalCountImages = Number(res.headers.get('pagination-count'))
+
+    return { catData, totalCountImages }
+}
+
