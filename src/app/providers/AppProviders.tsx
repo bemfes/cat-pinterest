408a69f4fc10fc39ccab67dff17988b20fa1be91
Anastasiia Ustin <2anastasiia@mail.ru>
2026-04-11 18:17:12 +0200

    added favourite cats context provider

--- src/app/providers/AppProviders.tsx
+++ src/app/providers/AppProviders.tsx
@@ -1,13 +1,14 @@
 import { RouterProvider } from "react-router-dom"
 import router from "../router"
+import FavouriteCatsContextProvider from "@/features/chooseFavourite/model"
 
 
 const AppProviders = ({ children }: { children: React.ReactNode }) => {
     return (
-       <> 
+       <FavouriteCatsContextProvider> 
          <RouterProvider router={router}/>
          {children}
-       </> 
+       </FavouriteCatsContextProvider> 
     )
 }
 
