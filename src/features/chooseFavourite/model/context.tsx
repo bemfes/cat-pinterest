6dfc75d80e29abc10391189ebc57e707412a57a8
Anastasiia Ustin <2anastasiia@mail.ru>
2026-04-11 17:57:50 +0200

    made local storage item for saving favourite cats

--- src/features/chooseFavourite/model/context.tsx
+++ src/features/chooseFavourite/model/context.tsx
@@ -1,12 +1,17 @@
 import { CatItemType } from "@/entities/cat/model";
+import { FAVOURITE_CATS_KEY } from "@/shared/constants";
 import { createContext } from "node:vm";
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 
 export const FavouriteCatsContext = createContext()
 
 const FavouriteCatsContextProvider: FC = ({children}) => {
 
     const [favouriteCats, setFavouriteCats] = useState<CatItemType[]>()
+
+    useEffect(() => {
+        localStorage.setItem(FAVOURITE_CATS_KEY, JSON.stringify(favouriteCats))
+    }, [favouriteCats])
 
     return (
         <FavouriteCatsContext.Provider value={{favouriteCats}}>
