f94e21541d744f0e95c57e404cbb31c34d678d5d
Anastasiia Ustin <2anastasiia@mail.ru>
2026-04-11 11:51:26 +0200

    created Button component

---
+++ src/shared/ui/Button/Button.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react"
+import { ButtonProps } from "./types"
+
+const Button: FC<ButtonProps> = ({children, className, onClick}) => {
+    return (
+        <button onClick={onClick} className={className}>{children}</button>
+    )
+}
+
+export default Button
