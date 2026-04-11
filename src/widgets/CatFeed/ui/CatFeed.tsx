be939d67f36ff121e728441912a3c71b9f4c2c23
Anastasiia Ustin <2anastasiia@mail.ru>
2026-04-11 14:58:36 +0200

    added grid for displaying cat images

--- src/widgets/CatFeed/ui/CatFeed.tsx
+++ src/widgets/CatFeed/ui/CatFeed.tsx
@@ -1,13 +1,19 @@
+import Grid from "@/shared/ui/Grid"
 import useGetCats from "../model/useGetCats"
+import CatItem from "@/entities/cat/ui/CatItem"
+import type { CatItemType } from "@/entities/cat/model"
 
 const CatFeed = () => {
     
-
    const { data } = useGetCats()
     
     return (
         <>
-        data
+        <Grid className="">
+           {data.map((item: CatItemType) => (
+                <CatItem key={item.id} url={item.url} id={item.id} />
+           ))}
+        </Grid>
         </>
     )
 }
