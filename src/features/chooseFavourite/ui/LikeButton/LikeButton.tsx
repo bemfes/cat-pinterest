c46b1b686c374156858c2dcd1c1cae55f6e52a84
Anastasiia Ustin <2anastasiia@mail.ru>
2026-04-11 17:28:52 +0200

    added classnames to Like Button

--- src/features/chooseFavourite/ui/LikeButton/LikeButton.tsx
+++ src/features/chooseFavourite/ui/LikeButton/LikeButton.tsx
@@ -1,11 +1,12 @@
 import Button from "@/shared/ui/Button"
 import { FC } from "react"
 import HeartIcon from "@/shared/assets/icons/heart-icon.svg?react"
+import styles from './LikeButton.module.css'
 
 const LikeButton: FC = () => {
   return (
-    <Button className="" onClick={() => console.log()}>
-      <HeartIcon />
+    <Button className={styles.button} onClick={() => console.log()}>
+      <HeartIcon className={styles.heart} />
     </Button>
   )
 }
