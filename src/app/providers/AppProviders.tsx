b61f4e0093fb460175b9d72123b10314c29aebc3
Anastasiia Ustin <2anastasiia@mail.ru>
2026-04-12 13:43:22 +0200

    deleted router

--- src/app/providers/AppProviders.tsx
+++ src/app/providers/AppProviders.tsx
@@ -1,14 +1,15 @@
-import { RouterProvider } from "react-router-dom"
-import router from "../router"
 import FavouriteCatsContextProvider from "@/features/chooseFavourite/model"
+import { ErrorBoundary } from "react-error-boundary"
+import Placeholder from "@/shared/ui/Placeholder"
 
 
 const AppProviders = ({ children }: { children: React.ReactNode }) => {
     return (
-       <FavouriteCatsContextProvider> 
-         <RouterProvider router={router}/>
-         {children}
-       </FavouriteCatsContextProvider> 
+       <ErrorBoundary fallback={<Placeholder to={ '/' } linkText="Вернуться к котикам 😸" text="Похоже, что-то пошло не так..."/>}>
+         <FavouriteCatsContextProvider> 
+           {children}
+         </FavouriteCatsContextProvider> 
+       </ErrorBoundary>
     )
 }
 
