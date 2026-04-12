7020c9c0da3352756dc21f443137f8264104c14a
Anastasiia Ustin <2anastasiia@mail.ru>
2026-04-12 13:20:57 +0200

    added Placeholder to Not Found page

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,10 +1,9 @@
+import Placeholder from "@/shared/ui/Placeholder"
 import { FC } from "react"
 
 const NotFoundPage: FC = () => {
     return (
-        <div>
-            not found
-        </div>
+        <Placeholder to={ "/" } linkText="Вернуться к котикам 😻" text="К сожалению, мы не нашли нужную вам страницу 😿"/>
     )
 }
 
