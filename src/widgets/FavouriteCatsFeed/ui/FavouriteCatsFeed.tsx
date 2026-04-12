234fec002ad139df40ce43fdfc98e58591f8794c
Anastasiia Ustin <2anastasiia@mail.ru>
2026-04-12 10:50:00 +0200

    added classnames for tags when there are no favourite cats

--- src/widgets/FavouriteCatsFeed/ui/FavouriteCatsFeed.tsx
+++ src/widgets/FavouriteCatsFeed/ui/FavouriteCatsFeed.tsx
@@ -12,9 +12,9 @@ const FavouriteCatsFeed: FC = () => {
     const {favouriteCats} = useFavouriteCatsContext()
 
     if (favouriteCats.length === 0) {
-        return <div>
-        <p>У вас пока нет любимых котиков 😿 <br/> Скорее выберите кого-то! 😸</p>
-        <AppLink to={'/'}>Выбрать любимых котиков 😻</AppLink>
+        return <div className={styles.dummy}>
+        <p className={styles.text}>У вас пока нет любимых котиков 😿 <br className={styles.br}/> Скорее выберите кого-то! 😸</p>
+        <AppLink className={styles.link} to={'/'}>Выбрать любимых котиков 😻</AppLink>
         </div>
     }
    
