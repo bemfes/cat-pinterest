2388aba6b7dd3f259506926c28848e87ee27189b
Anastasiia Ustin <2anastasiia@mail.ru>
2026-04-12 10:50:21 +0200

    added skeleton and loaing state for cat item

--- src/entities/cat/ui/CatItem/CatItem.tsx
+++ src/entities/cat/ui/CatItem/CatItem.tsx
@@ -1,12 +1,14 @@
-import { FC } from "react"
+import { FC, useState } from "react"
 import { CatItemProps } from "./types"
 import styles from './CatItem.module.css'
 
 
 const CatItem: FC<CatItemProps> = ({url, children}) => {
+    const [loading, setLoading] = useState<boolean>(true)
     return (
-       <div className={styles.item}>
-        <img className={styles.img} src={url} alt="adorable cat" />
+       <div className={`${styles.item} ${loading ? '' : styles.itemLoaded}`}>
+        {loading && <div className={styles.skeleton}></div>}
+        <img onLoad={() => setLoading(false)} className={styles.img} src={url} alt="adorable cat" />
         <div className={styles.wrapper}>
             {children}
         </div>
